54bdf8a23537c4a8c70589ab5828ebc36545c8db
Ulku <ulkuislamoglu@gmail.com>
2019-12-05 15:44:13 -0600

    test1

--- src/GitDeneme/firstProject.java
+++ src/GitDeneme/firstProject.java
@@ -4,5 +4,7 @@ public class firstProject {
 
     public static void main(String[] args) {
         System.out.println(" What are you doing");
+        System.out.println("%%^^&&^%&");
+        System.out.println("aahsjdhasjkdhf");
     }
 }
